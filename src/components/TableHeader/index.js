2370d19febf706ed21c627db763de32d354a804d
alexastef <62451633+alexastef@users.noreply.github.com>
2020-08-16 01:25:44 -0700

    Update index.js

--- src/components/TableHeader/index.js
+++ src/components/TableHeader/index.js
@@ -10,12 +10,12 @@ function TableHeader(props) {
                 <tr>
                     <th scope="col" className="align-middle">ID#</th>
                     <th scope="col" className="align-middle">Photo</th>
-                    <th scope="col" className="sortable both align-middle">First Name <button><i className="fas fa-sort" data-name="sort-first" onClick={props.handleSortClick}></i></button></th>
-                    <th scope="col" className="sortable both align-middle">Last Name <button><i className="fas fa-sort" data-name="sort-last"></i></button></th>
+                    <th scope="col" className="sortable both align-middle">First Name <button data-name="sort-first" onClick={props.handleSortClick}><i className="fas fa-sort"  ></i></button></th>
+                    <th scope="col" className="sortable both align-middle">Last Name <button data-name="sort-last" onClick={props.handleSortClick}><i className="fas fa-sort"></i></button></th>
                     <th scope="col" className="align-middle">Email</th>
                     <th scope="col" className="align-middle">Phone</th>
                     <th scope="col" className="align-middle">City</th>
-                    <th scope="col" className="align-middle">State <button><i className="fas fa-sort" data-name="sort-state"></i></button></th>
+                    <th scope="col" className="align-middle">State <button data-name="sort-location" onClick={props.handleSortClick}><i className="fas fa-sort"></i></button></th>
                 </tr>
             </thead>
     )
