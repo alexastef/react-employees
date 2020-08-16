6d4f4f101c34ee9f0180a146705b7be28be32f43
Alexa Stefankiewicz <alexa.c.stef@gmail.com>
2020-08-16 01:16:56 -0700

    changes were lost last time, remade and am readding

--- src/components/EmployeeContainer.js
+++ src/components/EmployeeContainer.js
@@ -4,41 +4,20 @@ import EmployeeTable from "./EmployeeTable";
 import API from "../utils/API";
 
 function EmployeeContainer() {
-  // this includes/returns the searchbar and the table, the table header and rows
-  // are injected into the table
 
-  // api call needs to be in here
-  // props are to be passed from the initial data response down through the children
-  // rows is the first component that will use the props
-  // props need to be passed from the data --> through the table --> to the row
 
   // Set initial search state to empty
   const [employees, setEmployees] = useState([]);
   const [filteredEmployees, setFilteredEmployees] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
 
-  // const [sortState, setSortState] = useState("descending");
-  // const [searchResults, setResultState] = useState([]);
 
   // When the page loads, get all the employees
   useEffect(() => {
     getAllEmployees();
 
-    //   const results = employees.filter(employee =>
-    //     employee.toLowerCase().includes(searchTerm));
-
-    //   setResultState(results);
-    // console.log("in useEffect: ", employees)
-
-    // ensure that first time it has employee data
-    //check if filtered array has something contain
-    // if (filteredEmployees) {
-    //     let filteredData = employees.filter((employee) => {
-    //         employee.name.first.includes(searchTerm)
-    //     })
-    //     setFilteredEmployees(filteredData)
-    // }
   }, []);
+
 
   function getFilteredArray(query) {
       // this is not the best way to sort by first and last name, it's a little
@@ -55,12 +34,6 @@ function EmployeeContainer() {
     setFilteredEmployees(filteredData);
   }
 
-  // useEffect(() => {
-  //     const results = employees.filter(employee => employee.name.first.toLowerCase().includes(searchTerm));
-  //     setResultState(results);
-  //     // setEmployees(results);
-
-  // }, [employees, searchTerm]);
 
   // get all the employees and set that state to the returned data
   function getAllEmployees() {
@@ -71,15 +44,6 @@ function EmployeeContainer() {
       .catch((err) => console.log(err));
   }
 
-  function handleSortClick(event) {
-    event.preventDefault();
-
-    // if (event.target.dataset.name === "sort-first") {
-    //     setSortState("descending");
-    // }
-    console.log(event.target.dataset);
-    //setstate filteredEmployee
-  }
 
   const handleInputChange = (event) => {
     setSearchTerm(event.target.value);
@@ -92,11 +56,9 @@ function EmployeeContainer() {
       <SearchBar handleInputChange={handleInputChange} value={searchTerm} />
 
       <EmployeeTable
-        handleSortClick={handleSortClick}
         employeeData={employees}
         filteredEmployee={filteredEmployees}
       />
-      {/* handleBtnClick={handleSortClick} */}
     </div>
   );
 }
